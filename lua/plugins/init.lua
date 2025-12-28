7dc7888e1c5f13067e8ff2284fa24ee9bcfc14db
kbgro <daniendegwa@gmail.com>
2025-12-28 13:23:35 +0300

    update the LuaSnip to v2

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -75,6 +75,7 @@ return {
             {
                 -- snippet plugin
                 "L3MON4D3/LuaSnip",
+                version = "v2.*",
                 dependencies = "rafamadriz/friendly-snippets",
                 opts = { history = true, updateevents = "TextChanged,TextChangedI" },
                 config = function(_, opts)
